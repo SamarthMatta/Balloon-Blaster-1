4300987938df7df3ca0a90272a3aa37e0b175ee4
SamarthMatta <91197695+SamarthMatta@users.noreply.github.com>
2021-11-23 15:07:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -56,7 +56,7 @@ function draw() {
    
   //creating continous enemies
 
-  // var select_balloon = Math.round(random(1,4));
+   var select_balloon = Math.round(random(1,4));
   // var select_balloon = random(1,4);
   // var select_balloon = Math.round(random());
   // var select_balloon = Math.round(random(1,4,2));
@@ -105,21 +105,21 @@ function draw() {
 //   red.scale = 0.1;
 //}
 
-// function redBalloon() {
-//   var red = createSprite(0,Math.round(random(20, 370)), 10, 10);
-//   red.addImage(red_balloonImage);
-//   red.velocityX = 3;
-//   red.lifetime = 150;
-//   red.scale = 0.1;
-// }
-
  function redBalloon() {
-   var red = createSprite(0, 10, Math.round(random(20, 370)) , 10);
+   var red = createSprite(0,Math.round(random(20, 370)), 10, 10);
    red.addImage(red_balloonImage);
-  red.velocityX = 3;
-  red.lifetime = 150;
+   red.velocityX = 3;
+   red.lifetime = 150;
    red.scale = 0.1;
  }
+
+// function redBalloon() {
+  // var red = createSprite(0, 10, Math.round(random(20, 370)) , 10);
+ //  red.addImage(red_balloonImage);
+ // red.velocityX = 3;
+ // red.lifetime = 150;
+//   red.scale = 0.1;
+// }
 
 function blueBalloon() {
   var blue = createSprite(0,Math.round(random(20, 370)), 10, 10);
